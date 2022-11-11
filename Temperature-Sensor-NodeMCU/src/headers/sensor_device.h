23ae989ed0c2440142c1a45d41083aa9ded50092
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-11-12 00:05:26 +0100

    developed sensor_device class further

--- Temperature-Sensor-NodeMCU/src/headers/sensor_device.h
+++ Temperature-Sensor-NodeMCU/src/headers/sensor_device.h
@@ -1,20 +1,47 @@
+#include <BLEDevice.h>
+#include <Arduino.h>
+
+#define CHARACTERISTIC_UUID "1adab8b2-100d-4dbd-9880-7ce062b02bd5"
+#define SERVICE_UUID "c8de7840-55e8-4a79-8286-0db52a0f1a94"
+#define GATEWAY_NAME "Gateway_ESP32"
 #define MINUTE 60000
+#define DEFAULT_MEASUREMENT_PER_MINUTE_VALUE 60
+
+#define DATA_PIN A0
+
+
+enum CalculationMode{
+    SlidingWindow,
+    Average,
+    Median,
+    Mode,
+};
 
 class SensorDevice
 {
 private:
     char *m_id;
+    bool m_buffer_is_ready;
     int m_measurements_per_min;
-    float m_current_temp_avg;
-    float m_previous_temp_avg;
+    int m_data_pointer;
+    float m_temp_avg;
     float *m_temp_buffer;
+    unsigned long m_last_data_time;
+    
+    CalculationMode m_calc_mode;
 
-    void ResizeBufferSize();
+    BLEDevice m_ble_device;
+
+    void ConnectToServer();
+    void ResizeBuffer();
+    float ConvertValueToDataEntry(uint16_t value);
 
 public:
-    SensorDevice(char *config);
+    SensorDevice();
     ~SensorDevice();
     char *GetId();
-    char *ToString();
-    void SetId(char *new_id);
+    bool canCollectData();
+    bool canSendData();
+    void CollecData();
+    void Sleep();
 };
--- Temperature-Sensor-NodeMCU/src/sensor_device.cpp
+++ Temperature-Sensor-NodeMCU/src/sensor_device.cpp
@@ -1,13 +1,59 @@
-#ifndef SENSOR_DEVICE_DEFINED
-#define SENSOR_DEVICE_DEFINED
 #include "headers/sensor_device.h"
-#endif
+#include "headers/uuid_generator.h"
 
-SensorDevice::SensorDevice(char *config)
+// Constructor
+SensorDevice::SensorDevice()
 {
+    Serial.begin(115200);
+    this->m_id = UUIDGenerator::generate_uuid();
+    this->m_buffer_is_ready = false;
+    this->ResizeBuffer();
+    this->m_measurements_per_min = DEFAULT_MEASUREMENT_PER_MINUTE_VALUE;
+    this->m_data_pointer = 0;
+    this->m_temp_avg = -1;
+    this->m_last_data_time = 0;
 }
 
-void SensorDevice::ResizeBufferSize()
+SensorDevice::~SensorDevice()
+{
+    if (this->m_temp_buffer != nullptr)
+    {
+        delete[] m_temp_buffer;
+    }
+}
+
+void SensorDevice::ResizeBuffer()
 {
     m_temp_buffer = new float[m_measurements_per_min];
+}
+
+void SensorDevice::Sleep(){
+
+    // Divedes the minute into the number of required measurements, then divides it by to prevent too long waits between measurements.
+    int time_to_sleep = MINUTE/this->m_measurements_per_min/2;
+    delay(time_to_sleep);
+}
+
+bool SensorDevice::canCollectData()
+{
+    return this->m_data_pointer < this->m_measurements_per_min;
+}
+
+bool SensorDevice::canSendData()
+{
+    unsigned long time_delta = millis() - this->m_last_data_time;
+    return time_delta >= MINUTE && this->m_data_pointer == this->m_measurements_per_min;
+}
+
+void SensorDevice::CollecData()
+{
+    float data = this->ConvertValueToDataEntry(analogRead(DATA_PIN));
+    this->m_temp_buffer[this->m_data_pointer] = data;
+    this->m_data_pointer++;
+}
+
+float SensorDevice::ConvertValueToDataEntry(uint16_t value)
+{
+    // TODO convert function to real data conversion
+    return (float)value;
 }
