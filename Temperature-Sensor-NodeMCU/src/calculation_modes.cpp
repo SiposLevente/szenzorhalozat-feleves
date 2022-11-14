49dde6b6fef08102834b80b9a657eff6bfaa25bb
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-11-14 15:02:16 +0100

    implemented calculatiom modes

--- Temperature-Sensor-NodeMCU/src/calculation_modes.cpp
+++ Temperature-Sensor-NodeMCU/src/calculation_modes.cpp
@@ -1,4 +1,6 @@
 #include "headers/calulation_modes.h"
+#include <bits/stdc++.h>
+using namespace std;
 
 float CalculationModes::Average(float *buffer, int size)
 {
@@ -12,8 +14,43 @@ float CalculationModes::Average(float *buffer, int size)
 
 float CalculationModes::Mode(float *buffer, int size)
 {
+    sort(buffer, buffer + size);
+
+    int number = buffer[0];
+    int mode = number;
+    int count = 1;
+    int countMode = 1;
+
+    for (int i = 1; i < size; i++)
+    {
+        if (buffer[i] == number)
+        {
+            ++count;
+        }
+        else
+        {
+            if (count > countMode)
+            {
+                countMode = count;
+                mode = number;
+            }
+            count = 1;
+            number = buffer[i];
+        }
+    }
 }
 
 float CalculationModes::Median(float *buffer, int size)
 {
+    sort(buffer, buffer + size);
+    if (size % 2 == 0)
+    {
+        return buffer[size / 2];
+    }
+    else
+    {
+        float num1 = buffer[size / 2];
+        float num2 = buffer[(size / 2) + 1];
+        return (num1 + num2) / 2.0;
+    }
 }
