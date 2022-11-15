22669ed6567adc46579937d70a7d248e7129a0ea
SiposLevente <sipi.levente@gmail.com>
2022-11-15 21:42:27 +0100

    bugfix, added ble related methods

--- Temperature-Sensor-NodeMCU/src/headers/sensor_device.h
+++ Temperature-Sensor-NodeMCU/src/headers/sensor_device.h
@@ -8,6 +8,10 @@
 
 #define DATA_PIN A16
 
+#define CALCULATION_MODE_AVERAGE_TYPE_TEXT "average"
+#define CALCULATION_MODE_MEDIAN_TYPE_TEXT "median"
+#define CALCULATION_MODE_MODE_TYPE_TEXT "mode"
+
 enum CalculationMode
 {
     Average,
@@ -27,7 +31,7 @@ private:
     float *m_temp_buffer;
     unsigned long m_last_data_time;
 
-    float (*m_calculation_mode_function)(float *, int);
+    float (*m_calculation_mode_function)(float *buffer, int size);
 
     CalculationMode m_calc_mode;
 
@@ -45,5 +49,7 @@ public:
     void Sleep();
     void CollecData();
     void ProcessData();
+    void SetMeasurementPerMinute(int new_measurement_per_minute);
+    void SetCalculationMode(CalculationMode calc_mode);
     float GetData();
 };
--- Temperature-Sensor-NodeMCU/src/main.cpp
+++ Temperature-Sensor-NodeMCU/src/main.cpp
@@ -3,11 +3,10 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-SensorDevice device;
+SensorDevice device = SensorDevice();
 
 void setup()
 {
-  device = SensorDevice();
 }
 
 void loop()
@@ -20,6 +19,33 @@ void loop()
   if (device.canSendData())
   {
     device.ProcessData();
+    device.GetData();
   }
   device.Sleep();
+}
+
+
+
+void SetDeviceMeasurementPerMinute(int new_measurement_per_minute)
+{
+  device.SetMeasurementPerMinute(new_measurement_per_minute);
+}
+
+void SetCalculationMode(char *new_calc_mode)
+{
+  CalculationMode calc_mode;
+  if (new_calc_mode == CALCULATION_MODE_AVERAGE_TYPE_TEXT)
+  {
+    calc_mode = CalculationMode::Average;
+  }
+  else if (new_calc_mode == CALCULATION_MODE_MEDIAN_TYPE_TEXT)
+  {
+    calc_mode = CalculationMode::Median;
+  }
+  else
+  {
+    calc_mode = CalculationMode::Mode;
+  }
+
+  device.SetCalculationMode(calc_mode);
 }
